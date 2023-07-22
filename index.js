626d6292a6cde03b92324d4bb44e65ed23b8482d
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-22 18:49:01 +0545

    Retiving Product Name

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
-const fs = require('fs');
-const json2csv = require('json2csv').Parser;
 
 const url = "https://stickersnepal.com/";
 
 (async () => {
-    let imdbdata = [];
-    const response = await request({
-        uri: url,
-        headers: {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-US,en;q=0.9"
-        },
-        gzip:true
-    })
+    try {
+        const porducts = [];
+        const response = await request({
+            uri: url,
+            headers: {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9"
+            },
+            gzip: true
+        });
 
-    let $ = cheerio.load(response);
-    let title = $('h2[class="h5 text-uppercase mb-2 mb-sm-3"]').text().trim();
-    console.log(title);
-
+        const $ = cheerio.load(response);
+        $('h5[class="mb-0 text-sm"] > a').each((index, element) => {
+            const productName = $(element).text().trim();
+            porducts.push({name:productName})
+        });
+        console.log(porducts);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 })();
